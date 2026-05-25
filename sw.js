00d9c038173b8ef2f5964b588952900ec32c02a6
TricMagicStick <4rdbyq7k5c@privaterelay.appleid.com>
2026-05-24 18:08:22 -0700

    Phase 0 started: Created manifest.json, sw.js, css/pokedex.css, and updated index.html with new structure. Foundation for multi-file v3.5.0.

---
+++ sw.js
@@ -0,0 +1,38 @@
+const CACHE_NAME = 'pokedex-v3.5.0';
+const urlsToCache = [
+  './',
+  './index.html',
+  './manifest.json',
+  './css/pokedex.css'
+  // JS files will be added in later phases
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
